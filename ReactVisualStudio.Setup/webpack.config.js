4b1341889a4dbb4dd88f76771945d1da86bf7d24
mikeborowy <mikeborowy@gmail.com>
2017-05-30 22:38:53 +0200

    updated project

--- ReactVisualStudio.Setup/webpack.config.js
+++ ReactVisualStudio.Setup/webpack.config.js
@@ -45,19 +45,24 @@ module.exports = {
  
         { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
         // Font Definitions
-        { test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]' },
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/vnd.ms-fontobject&name=fonts/[name].[ext]' },
-        { test: /\.[ot]tf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]' },
-        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml&name=fonts/[name].[ext]' },
+        { test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff&name=../fonts/[name].[ext]' },
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/vnd.ms-fontobject&name=../fonts/[name].[ext]' },
+        { test: /\.[ot]tf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream&name=../fonts/[name].[ext]' },
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml&name=../fonts/[name].[ext]' },
         //Images
         {
             test: /\.(jpg|jpeg|gif|png)$/,
-            exclude: /node_modules/,
-            loaders: [
-                'file?name=../images/[name].[ext]',
-                'image-webpack?progressive=true&optimizationLevel=7&interlaced=true'
-            ]
-        }
+            exclude: /(node_modules)/,
+            loader: "file-loader?name=../images/[name].[ext]"
+        },
+        //{
+        //    test: /\.(jpg|jpeg|gif|png)$/,
+        //    exclude: /node_modules/,
+        //    loaders: [
+        //        'file?name=../images/[name].[ext]',
+        //        'image-webpack-loader?progressive=true&optimizationLevel=7&interlaced=true'
+        //    ]
+        //}
         ]
     }
 };
